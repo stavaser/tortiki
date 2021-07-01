643b4815b4dcb6dc0aaf5fa1d91380734b6b85c1
Valeria Starkova <stavaser@icloud.com>
2021-07-01 10:45:32 +0900

    test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import configureStore from './redux/store';
+import configureStore from './_store';
 
 const store = configureStore();
 
