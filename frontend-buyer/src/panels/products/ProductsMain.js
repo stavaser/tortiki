25f12d4bd5ca3e2d8572ed67ab29f6185eca5f6a
Valeria Starkova <stavaser@icloud.com>
2021-07-03 20:05:36 +0900

    added product filters

--- frontend-buyer/src/panels/products/ProductsMain.js
+++ frontend-buyer/src/panels/products/ProductsMain.js
@@ -1,4 +1,4 @@
-import { Icon24Add } from '@vkontakte/icons';
+import { Icon24Filter, Icon24FavoriteOutline } from '@vkontakte/icons';
 import {
   CardGrid,
   CellButton,
@@ -14,9 +14,13 @@ import {
   Card,
   HorizontalScroll,
   HorizontalCell,
+  SubnavigationBar,
+  SubnavigationButton,
+  Counter,
 } from '@vkontakte/vkui';
+
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import cake from '../../assets/cake.jpeg';
 
 import * as to from '../../navigation/products';
@@ -28,74 +32,108 @@ const largeImageStyles = {
   border: '1px solid var(--placeholder_icon_background)',
   objectFit: 'cover',
 };
+const categories = ['Торты', 'Капкейки', 'Пицца', 'Суши'];
+const ProductsMain = ({ id, go, openModal, filtersCount }) => {
+  const [category, setCategory] = useState(false);
 
-const ProductsMain = ({ id, go }) => (
-  <Panel id={id}>
-    <PanelHeader>Продукты</PanelHeader>
-    <Group mode="plain" header={<Header>Популярное</Header>}>
-      <CardScroll size="m" style={{ margin: '-8px' }}>
-        {[...Array.from({ length: 30 }, (v, i) => i)].map((i) => (
-          <Card size="l" mode="shadow" style={{ margin: '8px' }}>
-            <div style={{ height: 96, padding: '10px', display: 'flex' }}>
-              <Avatar size={96} mode="image" src={cake} />
-              <div style={{ marginLeft: '16px' }}>
-                <p>Торт 'красный бархат'</p>
-                <Button mode="outline">1200 ₽</Button>
+  const selectCategory = (category) => {
+    setCategory(category);
+  };
+  return (
+    <Panel id={id}>
+      <PanelHeader>Продукты</PanelHeader>
+      <Group mode="plain" header={<Header>Популярное</Header>}>
+        <CardScroll size="m">
+          {[...Array.from({ length: 30 }, (v, i) => i)].map((i) => (
+            <Card size="l" mode="shadow" style={{ margin: '8px' }}>
+              <div style={{ height: 96, padding: '10px', display: 'flex' }}>
+                <Avatar size={96} mode="image" src={cake} />
+                <div style={{ marginLeft: '16px' }}>
+                  <p>Торт 'красный бархат'</p>
+                  <Button mode="outline">1200 ₽</Button>
+                </div>
               </div>
-            </div>
-          </Card>
-        ))}
-      </CardScroll>
-    </Group>
-    <Group mode="plain" header={<Header>Продукты</Header>}>
-      <CardGrid size="m">
-        <ContentCard
-          id="0"
-          onClick={go}
-          data-nav={to.PRODUCTS_DETAIL}
-          image={cake}
-          subtitle="Торт 'красный бархат'"
-          header="1200 руб"
-          caption="1200 гр"
-          maxHeight={100}
-        />
-        <ContentCard
-          id="1"
-          onClick={go}
-          data-nav={to.PRODUCTS_DETAIL}
-          image={cake}
-          subtitle="Торт 'красный бархат'й бархат'й бархат'"
-          header="1200 руб"
-          caption="1200 гр"
-          maxHeight={100}
-        />
-        <ContentCard
-          id="2"
-          onClick={go}
-          data-nav={to.PRODUCTS_DETAIL}
-          image={cake}
-          subtitle="Торт 'красный бархат'й бархат'й бархат'"
-          header="1200 руб"
-          caption="1200 гр"
-          maxHeight={100}
-        />
-      </CardGrid>
-    </Group>
-    <Group mode="plain" header={<Header>Архив (1)</Header>}>
-      <CardGrid size="m">
-        <ContentCard
-          onClick={go}
-          data-nav={to.PRODUCTS_DETAIL}
-          image={cake}
-          subtitle="Торт 'красный бархат'"
-          header="1200 руб"
-          caption="1200 гр"
-          maxHeight={100}
-        />
-      </CardGrid>
-    </Group>
-  </Panel>
-);
+            </Card>
+          ))}
+        </CardScroll>
+      </Group>
+      <Group>
+        <SubnavigationBar>
+          <SubnavigationButton
+            before={<Icon24Filter />}
+            selected={filtersCount > 0}
+            expandable
+            after={
+              filtersCount > 0 && (
+                <Counter mode="primary" size="s">
+                  {filtersCount}
+                </Counter>
+              )
+            }
+            onClick={openModal}
+          >
+            Фильтры
+          </SubnavigationButton>
+          {categories.map((item) => (
+            <SubnavigationButton
+              selected={item === category}
+              onClick={() => selectCategory(item)}
+            >
+              {item}
+            </SubnavigationButton>
+          ))}
+        </SubnavigationBar>
+      </Group>
+      <Group mode="plain" header={<Header>Продукты</Header>}>
+        <CardGrid size="m">
+          <ContentCard
+            id="0"
+            onClick={go}
+            data-nav={to.PRODUCTS_DETAIL}
+            image={cake}
+            subtitle="Торт 'красный бархат'"
+            header="1200 руб"
+            caption="1200 гр"
+            maxHeight={100}
+          />
+          <ContentCard
+            id="1"
+            onClick={go}
+            data-nav={to.PRODUCTS_DETAIL}
+            image={cake}
+            subtitle="Торт 'красный бархат'й бархат'й бархат'"
+            header="1200 руб"
+            caption="1200 гр"
+            maxHeight={100}
+          />
+          <ContentCard
+            id="2"
+            onClick={go}
+            data-nav={to.PRODUCTS_DETAIL}
+            image={cake}
+            subtitle="Торт 'красный бархат'й бархат'й бархат'"
+            header="1200 руб"
+            caption="1200 гр"
+            maxHeight={100}
+          />
+        </CardGrid>
+      </Group>
+      <Group mode="plain" header={<Header>Архив (1)</Header>}>
+        <CardGrid size="m">
+          <ContentCard
+            onClick={go}
+            data-nav={to.PRODUCTS_DETAIL}
+            image={cake}
+            subtitle="Торт 'красный бархат'"
+            header="1200 руб"
+            caption="1200 гр"
+            maxHeight={100}
+          />
+        </CardGrid>
+      </Group>
+    </Panel>
+  );
+};
 
 ProductsMain.propTypes = {
   id: PropTypes.string.isRequired,
--- frontend-buyer/src/views/ProductsView.js
+++ frontend-buyer/src/views/ProductsView.js
@@ -1,18 +1,46 @@
 import React, { useState } from 'react';
-import { View } from '@vkontakte/vkui';
+import {
+  View,
+  Button,
+  FormItem,
+  Checkbox,
+  PanelHeaderButton,
+  PanelHeaderClose,
+  ModalPageHeader,
+  ModalCard,
+  ModalRoot,
+  FormLayout,
+} from '@vkontakte/vkui';
+import { Icon24Dismiss } from '@vkontakte/icons';
 import ProductsMain from '../panels/products/ProductsMain';
 import ProductsDetail from '../panels/products/ProductsDetail';
 import AddProduct from '../panels/products/AddProduct';
 import EditProducts from '../panels/products/EditProduct';
-// import {
-//   PRODUCTS_ADD,
-//   PRODUCTS_EDIT,
-//   PRODUCTS_DETAIL,
-//   PRODUCTS_MAIN,
-// } from '../navigation/products';
+
 import * as to from '../navigation/products';
+const MODAL_NAME = 'filters';
+
+const FILTERS_SIZE = [
+  { value: 36, label: 36 },
+  { value: 37, label: 37 },
+  { value: 38, label: 38 },
+  { value: 39, label: 39 },
+];
+
+const FILTERS_STYLE = [
+  { value: 'Вечерний', label: 'Вечерний' },
+  { value: 'Деловой', label: 'Деловой' },
+  { value: 'Повседневный', label: 'Повседневный' },
+  { value: 'Спортивный', label: 'Спортивный' },
+];
 const ProductsView = ({ id }) => {
   const [activePanel, setActivePanel] = useState(to.PRODUCTS_MAIN);
+
+  const [filtersModalOpened, setFiltersModalOpened] = useState(false);
+  const [filtersCount, setFiltersCount] = useState(0);
+
+  const [filterSizes, setFilterSizes] = useState([36]);
+  const [filterStyles, setFilterStyles] = useState(['Вечерний']);
 
   const go = (e) => {
     const target = e.target.dataset.nav;
@@ -24,10 +52,80 @@ const ProductsView = ({ id }) => {
       setActivePanel(target || currentTarget);
     }
   };
+  const openModal = () => {
+    setFiltersModalOpened(true);
+  };
 
+  const closeModal = () => {
+    setFiltersModalOpened(false);
+  };
+
+  const onChangeFilterSize = (e) => {
+    const { value, checked } = e.currentTarget;
+    if (checked) {
+      setFilterSizes([...filterSizes, +value]);
+    } else {
+      setFilterSizes(filterSizes.filter((v) => v !== +value));
+    }
+  };
+
+  const onChangeFilterStyle = (e) => {
+    const { value, checked } = e.currentTarget;
+    if (checked) {
+      setFilterStyles([...filterStyles, value]);
+    } else {
+      setFilterStyles(filterStyles.filter((v) => v !== value));
+    }
+  };
+
+  const applyFilters = () => {
+    let count = 0;
+
+    filterSizes.length && count++;
+    filterStyles.length && count++;
+
+    closeModal();
+    setFiltersCount(count);
+  };
+
+  const modal = (
+    <ModalRoot
+      activeModal={filtersModalOpened ? MODAL_NAME : null}
+      onClose={closeModal}
+    >
+      <ModalCard
+        id={MODAL_NAME}
+        header={'Фильтры'}
+        actions={
+          <Button size="l" mode="primary" onClick={applyFilters}>
+            Показать результаты
+          </Button>
+        }
+      >
+        <FormLayout>
+          <FormItem>
+            <Checkbox>Есть доставка на дом</Checkbox>
+            <Checkbox>Есть доставка в другие регионы</Checkbox>
+            <Checkbox>Еще что-то...</Checkbox>
+          </FormItem>
+
+          {/* <FormItem>
+            <Button size="l" stretched onClick={applyFilters}>
+              Показать результаты
+            </Button>
+          </FormItem> */}
+        </FormLayout>
+      </ModalCard>
+    </ModalRoot>
+  );
   return (
-    <View activePanel={activePanel} id={id}>
-      <ProductsMain id={to.PRODUCTS_MAIN} go={go} />
+    <View activePanel={activePanel} id={id} modal={modal}>
+      <ProductsMain
+        id={to.PRODUCTS_MAIN}
+        go={go}
+        filtersCount={filtersCount}
+        openModal={openModal}
+      />
       <ProductsDetail id={to.PRODUCTS_DETAIL} go={go} />
       <AddProduct id={to.PRODUCTS_ADD} go={go} />
       <EditProducts id={to.PRODUCTS_EDIT} go={go} />
