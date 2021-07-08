916038a7503674a2ff8ba0bbe107ba4f0f270003
Valeria Starkova <stavaser@icloud.com>
2021-07-09 04:32:45 +0900

    changed product card style

--- frontend-buyer/src/components/products/ProductCard.js
+++ frontend-buyer/src/components/products/ProductCard.js
@@ -6,6 +6,8 @@ import PropTypes from 'prop-types';
 import * as to from '../../navigation/products';
 import { useSelector, useDispatch } from 'react-redux';
 import { PRODUCT_ID_SET } from '../../redux/constants/products.constants';
+import { CellButton } from '@vkontakte/vkui';
+import { Button } from 'react-scroll';
 
 const ProductCard = ({ go, to, data, product_id }) => {
   const dispatch = useDispatch();
@@ -21,11 +23,29 @@ const ProductCard = ({ go, to, data, product_id }) => {
     <ContentCard
       onClick={(e) => onClick(e)}
       data-nav={to}
+      maxHeight={400}
       product_id={product_id}
       image={data.image}
-      subtitle={data.title}
-      header={data.price}
-      caption={data.weight}
+      text={data.title}
+      // header={data.price}
+      caption={
+        <React.Fragment>
+          {data.weight + ' г'}
+          <div
+            style={{
+              marginTop: '16px',
+              // width: '100%',
+              backgroundColor: 'rgba(0,0,0,0.08)',
+              borderRadius: '5px',
+              textAlign: 'center',
+              padding: '8px',
+              color: 'black',
+            }}
+          >
+            860 ₽
+          </div>
+        </React.Fragment>
+      }
       maxHeight={100}
     />
   );
